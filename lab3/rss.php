350186bab40a160a6cb4fff22fbb8e8be2df6807
Khairullina16 <81875457+Khairullina16@users.noreply.github.com>
2022-05-16 00:59:23 +0500

    ЛР3

---
+++ lab3/rss.php
@@ -0,0 +1,15 @@
+﻿<?php
+   $tek=$_GET["tek"];
+   $rss = simplexml_load_file("index.rss");      
+   $s=0;
+   foreach ($rss->channel->item as $item) {
+       $s++;
+	   echo "<div style='width: 1800px; border: 1px solid black; background-color: #F5FFFA'>";
+       echo '<h2 style="font-weight: bold">'.$item->title.'</h2>';
+       echo '<p><big>'.$item->description.'</big></p>'; 
+       echo "<img src=\"". (string)$item->enclosure['url'][0]. "\" style='width: 600px'></div><br>";
+       if($s==$tek) {
+          break;
+         }
+   }
+?>
